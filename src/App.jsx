1245638252e06c6da39552d30ec029b0a428cb9f
Saroj Padhan <40264980+saroj8455@users.noreply.github.com>
2023-12-11 17:25:36 +0530

    implement footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import React, { useState } from 'react';
 import logo from './assets/logo.png';
 import videoPreview from './assets/video-preview.png';
+import whiteLogo from './assets/logo-white.png';
 function App() {
   return (
     <>
@@ -194,6 +195,179 @@ function App() {
           </p>
         </div>
       </section>
+      {/* FAQ */}
+      <section className='faq bg-light'>
+        <div className='container-sm'>
+          <h3 className='text-xl text-center'>Frequently Asked Questions</h3>
+          <ul className='faq-menu'>
+            <li className='active'>All</li>
+            <li>Getting Started</li>
+            <li>Pricing</li>
+          </ul>
+          <div className='faq-content'>
+            <div className='faq-group'>
+              <div className='faq-group-header'>
+                <h4 className='text-md'>
+                  How does the contact management feature help me keep track of
+                  clients and partners?
+                </h4>
+                <i className='fas fa-minus' />
+              </div>
+              <div className='faq-group-body open'>
+                <p>
+                  Keep track of your contacts, gain valuable insights, analyse
+                  live data and performance metrics.
+                </p>
+              </div>
+            </div>
+            <div className='faq-group'>
+              <div className='faq-group-header'>
+                <h4 className='text-md'>
+                  Can I customize the dashboards and reporting feature to
+                  display the metrics that are most important to my business?
+                </h4>
+                <i className='fas fa-plus' />
+              </div>
+              <div className='faq-group-body'>
+                <p>
+                  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad
+                  culpa enim blanditiis rem ipsum aliquam, unde iste fugit
+                  praesentium eos?
+                </p>
+              </div>
+            </div>
+            <div className='faq-group'>
+              <div className='faq-group-header'>
+                <h4 className='text-md'>
+                  How does the task management feature help me delegate tasks to
+                  my team and track progress?
+                </h4>
+                <i className='fas fa-plus' />
+              </div>
+              <div className='faq-group-body'>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Incidunt, a! Quaerat, voluptatum. Animi molestias ex quasi
+                  explicabo minima perferendis commodi.
+                </p>
+              </div>
+            </div>
+            <div className='faq-group'>
+              <div className='faq-group-header'>
+                <h4 className='text-md'>
+                  Can I collaborate with my team in real-time using all tools?
+                </h4>
+                <i className='fas fa-plus' />
+              </div>
+              <div className='faq-group-body'>
+                <p>
+                  Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                  Obcaecati doloremque assumenda aut dolorem recusandae
+                  quibusdam aliquid. Repellat animi quam vitae.
+                </p>
+              </div>
+            </div>
+            <div className='faq-group'>
+              <div className='faq-group-header'>
+                <h4 className='text-md'>
+                  Is the app available on all devices?
+                </h4>
+                <i className='fas fa-plus' />
+              </div>
+              <div className='faq-group-body'>
+                <p>
+                  Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                  Obcaecati doloremque assumenda aut dolorem recusandae
+                  quibusdam aliquid. Repellat animi quam vitae.
+                </p>
+              </div>
+            </div>
+            <div className='faq-group'>
+              <div className='faq-group-header'>
+                <h4 className='text-md'>
+                  How does the app help me stay compliant when working with
+                  freelancers and contractors?
+                </h4>
+                <i className='fas fa-plus' />
+              </div>
+              <div className='faq-group-body'>
+                <p>
+                  Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                  Obcaecati doloremque assumenda aut dolorem recusandae
+                  quibusdam aliquid. Repellat animi quam vitae.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      {/* Footer */}
+      <footer className='footer bg-black'>
+        <div className='container'>
+          <div className='footer-grid'>
+            <div>
+              <a href='index.html'>
+                <img src={whiteLogo} alt='logo' />
+              </a>
+              <div className='card'>
+                <h4>Subscribe to Newsletter</h4>
+                <p className='text-sm'>
+                  Subscribe now to receive tips on how to take your business to
+                  the next level.
+                </p>
+                <form>
+                  <input
+                    type='email'
+                    id='email'
+                    placeholder='Enter your email'
+                  />
+                  <button type='submit' className='btn btn-primary btn-block'>
+                    Subscribe
+                  </button>
+                </form>
+              </div>
+              <i className='fab fa-linkedin' />
+              <i className='fab fa-twitter' />
+            </div>
+            <div>
+              <h4>Company</h4>
+              <ul>
+                <li>
+                  <a href='#'>About Us</a>
+                </li>
+                <li>
+                  <a href='#'>Our Process</a>
+                </li>
+                <li>
+                  <a href='#'>Join Our Team</a>
+                </li>
+              </ul>
+            </div>
+            <div>
+              <h4>Resources</h4>
+              <ul>
+                <li>
+                  <a href='#'>News</a>
+                </li>
+                <li>
+                  <a href='#'>Research</a>
+                </li>
+                <li>
+                  <a href='#'>Recent Projects</a>
+                </li>
+              </ul>
+            </div>
+            <div>
+              <h4>Contact</h4>
+              <ul>
+                <li>
+                  <a href='#'>hello@growthapp.com</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </footer>
     </>
   );
 }
