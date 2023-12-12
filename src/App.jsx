171b86d4ec0dac67845bc5058493afcc1f47d278
Saroj Padhan <40264980+saroj8455@users.noreply.github.com>
2023-12-12 17:49:56 +0530

    implement landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,61 @@
 // eslint-disable-next-line no-unused-vars
 import React, { useState } from 'react';
-
+import bookLogo from './assets/book_logo.png';
 function App() {
   return (
     <>
-      <h1>Book Template UI</h1>
+      <nav className='navbar'>
+        <div className='container'>
+          <div className='logo'>
+            <a href='index.html'>
+              <img src={bookLogo} height={100} width={150} alt='logo' />
+            </a>
+          </div>
+
+          <div className='main-menu'>
+            <ul>
+              <li>
+                <a href='index.html'>Home</a>
+              </li>
+              <li>
+                <a href='#'>About Us</a>
+              </li>
+              <li>
+                <a href='#'>Blog</a>
+              </li>
+              <li>
+                <a className='btn' href='#'>
+                  <i className='fas fa-user'></i> Log In
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      {/* Books banner */}
+      <section className='hero'>
+        <div className='container'>
+          <div className='hero-content'>
+            <h1 className='hero-heading  text-7xl'>
+              Make CTAs visually prominent to drive user engagement.
+            </h1>
+            <p className='hero-text'>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim,
+              laborum? Asperiores quo eos, odit laboriosam ut, qui quidem,
+              architecto nihil quia modi dolor amet quos tempora ipsa id non
+              iste!
+            </p>
+            <div className='hero-buttons flex gap-2 align-items-center '>
+              <a href='#' className='btn btn-primary'>
+                Get Started
+              </a>
+              <a href='#' className='btn'>
+                <i className='fas fa-laptop' /> Book a Demo
+              </a>
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   );
 }
